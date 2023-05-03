b73c9fb5d3bdc0be23d879856ce5e72c5e138d97
mikejhouseman <mikej.houseman@gmail.com>
2023-05-02 19:52:12 -0700

    phase 1 complete

---
+++ server/db/migrations/20230503023246-create-image.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      url: {
+        type: Sequelize.STRING,
+        notEmpty: true,
+        allowNull: false
+      },
+      imageableType: {
+        type: Sequelize.ENUM('UserProfile', 'BlogPost'),
+        notEmpty: true
+      },
+      imageableId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Images');
+  }
+};
